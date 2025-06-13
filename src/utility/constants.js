c0468b274541ee0c1b70d3a6f000106673054e8d
NILESH <nileshnimawat121@gmail.com>
2025-06-13 17:05:10 +0530

    production setup

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,23 +1,22 @@
-export const ALL_USERS_TWEETS = "/api/v1/tweet/getAllTweets";
-export const ALL_OTHER_USERS = "/api/v1/user/getAllUsers";
-export const CREATE_TWEET = "/api/v1/tweet/createTweet"
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
+export const ALL_USERS_TWEETS = `${BASE_URL}/api/v1/tweet/getAllTweets`;
+export const ALL_OTHER_USERS = `${BASE_URL}/api/v1/user/getAllUsers`;
+export const CREATE_TWEET = `${BASE_URL}/api/v1/tweet/createTweet`;
 
-//auth
-export const LOGOUT = "/api/v1/user/logout"
-export const LOGIN = "/api/v1/user/login"
-export const SIGNUP = "/api/v1/user/signup"
+// Auth
+export const LOGOUT = `${BASE_URL}/api/v1/user/logout`;
+export const LOGIN = `${BASE_URL}/api/v1/user/login`;
+export const SIGNUP = `${BASE_URL}/api/v1/user/signup`;
 
-export const MY_PROFILE = "/api/v1/user/myprofile"
+export const MY_PROFILE = `${BASE_URL}/api/v1/user/myprofile`;
+export const INDIVIDUAL_TWEETS = `${BASE_URL}/api/v1/tweet/getIndividualTweets/`;
 
-export const INDIVIDUAL_TWEETS = "/api/v1/tweet/getIndividualTweets/"
+export const USER_END_POINT = `${BASE_URL}/api/v1/user/`;
+export const TWEET_END_POINT = `${BASE_URL}/api/v1/tweet/`;
 
+export const FOLLOW = `${BASE_URL}/api/v1/user/follow`;
+export const UNFOLLOW = `${BASE_URL}/api/v1/user/unfollow`;
 
-export const USER_END_POINT = "/api/v1/user/"
-export const TWEET_END_POINT = "/api/v1/tweet/"
+export const SEARCH = `${BASE_URL}/api/v1/user/search?query=`;
 
-
-export const FOLLOW = "/api/v1/user/follow"
-export const UNFOLLOW = "/api/v1/user/unfollow"
-
-export const SEARCH = "/api/v1/user/search?query="
